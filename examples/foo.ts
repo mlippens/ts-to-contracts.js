ba0ecd998dfe9311301bedd98a92664400664973
froginvasion <lippens.m@gmail.com>
2014-03-12 10:52:04 +0100

    support for optional types on parameters

--- examples/foo.ts
+++ examples/foo.ts
@@ -1,5 +1,8 @@
-var foo = function (a: (b: number)=> string): string { return "foo";};
+var foo = function (a?: (b: number)=> string): string { return "foo";};
 var bar = function(): any {};
 var foobar = function(a: boolean):void { dosomethingcomplicated(); };
+
+(function(a?: string){});
+
 
 
