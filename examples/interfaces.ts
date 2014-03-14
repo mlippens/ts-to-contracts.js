f6ca2aece496a35e14828faaca49e51357c3ac54
froginvasion <lippens.m@gmail.com>
2014-03-14 22:05:21 +0100

    support for interfaces (no extends yet)

--- examples/interfaces.ts
+++ examples/interfaces.ts
@@ -2,3 +2,10 @@ interface Foo {
     bar?: string;
     foo: boolean;
 }
+
+interface PersistenceOptions {
+    url?: string;
+    beforeSend?: (jqxhr: JQueryXHR) => void;
+    success?: (modelOrCollection?: any, response?: any, options?: any) => void;
+    error?: (modelOrCollection?: any, jqxhr?: JQueryXHR, options?: any) => void;
+}
