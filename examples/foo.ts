cc2b9096b170c189619d4b9d9ced340c061f3591
froginvasion <lippens.m@gmail.com>
2014-03-15 12:37:30 +0100

    support for function declarations added header file added node exec that links to proxies and collections from harmony

--- examples/foo.ts
+++
@@ -1,8 +0,0 @@
-var foo = function (a?: (b: number)=> string): string { return "foo";};
-var bar = function(): any {};
-var foobar = function(a: boolean):void { dosomethingcomplicated(); };
-
-(function(a?: string){});
-
-
-
---
+++ examples/functions.ts
@@ -0,0 +1,5 @@
+function foo(a: number):number {
+	return "2";
+}
+
+foo(2);
