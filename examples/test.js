ee24ad03d32fc9ce9aa6191ec72e8a4f314de112
froginvasion <lippens.m@gmail.com>
2014-04-07 14:31:16 +0200

    support both call and newhandlers in interfaces.

--- examples/test.js
+++ examples/test.js
@@ -8,25 +8,11 @@
     }
 
     var globals = {};
-    var IA = C.object({
-        bar: C.fun([], C.Any, {})
-    }, {});
-    var IB = C.object({
-        bar: C.fun([], C.Any, {}),
-        foo: C.fun([], C.Any, {})
-    }, {});
-    //IB implicitly extends interface IA due to Typescript's structural typing
-
-    globals.fn = C.guard(C.fun([C.fun([IA], C.Undefined, {})], C.Any, {}), function(passedFn) {
-        var a = C.guard(IA, {
-            bar: C.guard(C.fun([], C.Any, {}), function() {}, "main")
-        }, "main");
-        passedFn(a);
-    }, "main")
-
-    //TypeError: b.foo is not a functionfn
-    (C.guard(C.fun([IB], C.Any, {}), function(b) {
-        b.foo();
-    }, "main"));
+    var Foo = C.and(C.object({
+        foo: C.Str
+    }, {}), C.fun({
+        "call": [C.Str, C.Any],
+        "new": [, C.Undefined]
+    }));
     return globals;
 }).call(this);
--- examples/test.ts
+++ examples/test.ts
@@ -1,18 +1,5 @@
-interface IA {
-    bar(): void
+interface Foo {
+    ():void;
+    new(a: string): any;
+    foo : string;
 }
-interface IB {
-    bar(): void;
-    foo(): void;
-}
-//IB implicitly extends interface IA due to Typescript's structural typing
-
-function fn(passedFn: (a: IA) => void) {
-    var a:IA = {bar: function(){}};
-    passedFn(a);
-}
-
-//TypeError: b.foo is not a functionfn
-(function(b: IB){
-    b.foo();
-});
