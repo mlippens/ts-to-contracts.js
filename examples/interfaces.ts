cbbf9f07c3b55ce498a44faa7d65fc3c4bcff831
froginvasion <lippens.m@gmail.com>
2014-04-08 16:14:04 +0200

    added extending interfaces support; added hoisting of interface variables to the top;

--- examples/interfaces.ts
+++ examples/interfaces.ts
@@ -1,51 +1,24 @@
-// Type definitions for mocha 1.17.1
-// Project: http://visionmedia.github.io/mocha/
-// Definitions by: Kazi Manzur Rashid <https://github.com/kazimanzurrashid/>
-// DefinitelyTyped: https://github.com/borisyankov/DefinitelyTyped
-
-interface Mocha {
-    // Setup mocha with the given setting options.
-    setup(options: MochaSetupOptions): Mocha;
-
-    //Run tests and invoke `fn()` when complete.
-    run(callback?: () => void): void;
-
-    // Set reporter as function
-    reporter(reporter: () => void): Mocha;
-
-    // Set reporter, defaults to "dot"
-    reporter(reporter: string): Mocha;
-
-    // Enable growl support.
-    growl(): Mocha;
+interface ModelSetOptions extends Silenceable, Validable {
 }
 
-interface MochaSetupOptions {
-    //milliseconds to wait before considering a test slow
-    slow?: number;
-
-    // timeout in milliseconds
-    timeout?: number;
-
-    // ui name "bdd", "tdd", "exports" etc
-    ui?: string;
-
-    //array of accepted globals
-    globals?: any;
-
-    // reporter instance (function or string), defaults to `mocha.reporters.Dot`
-    reporter?: any;
-
-    // bail on the first test failure
-    bail?: Boolean;
-
-    // ignore global leaks
-    ignoreLeaks?: Boolean;
-
-    // grep string or regexp to filter tests with
-    grep?: any;
+interface ModelFetchOptions extends PersistenceOptions, ModelSetOptions, Parseable {
 }
 
-interface MochaDone {
-    (error?: Error): void;
+interface Silenceable {
+    silent?: boolean;
+}
+
+interface Validable {
+    validate?: boolean;
+}
+
+interface Parseable {
+    parse?: any;
+}
+
+interface PersistenceOptions {
+    url?: string;
+    beforeSend?: (jqxhr: JQueryXHR) => void;
+    success?: (modelOrCollection?: any, response?: any, options?: any) => void;
+    error?: (modelOrCollection?: any, jqxhr?: JQueryXHR, options?: any) => void;
 }
